595b17267422021bc043992d177dcfecef45f153
Mikhail Leonov <mleonovvv@gmail.com>
2018-03-26 00:30:13 +0300

    Edit image-family

--- packer/scripts/create-redditvm.sh
+++ packer/scripts/create-redditvm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gcloud compute instances create reddit-app-full \
  --boot-disk-size=10GB \
- --image=reddit-full-1522010322 \
+ --image-family=reddit-full\
  --machine-type=g1-small \
  --tags=puma-server \
  --restart-on-failure
